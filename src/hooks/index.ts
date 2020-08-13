1f5e019fb343b7c69c9485c9d07998e3a4118e5b
Hans <1019317107@qq.com>
2020-08-13 11:47:48 +0800

    fix: add import

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,18 +2,21 @@
  * @Date: 2020-05-27 16:16:10
  * @LastEditors: Hans
  * @description:
- * @LastEditTime: 2020-08-12 16:09:32
+ * @LastEditTime: 2020-08-13 11:42:27
  * @FilePath: /hooooks/src/hooks/index.ts
  */
 
 import useDidMount from "./useDidMount";
 import useDidUpdate from "./useDidUpdate";
 import useWillUnMount from "./useWillUnMount";
+import useUpdate from "./useUpdate";
+import useIsFirstMount from "./useIsFirstMount";
+
 import useClickOutside from "./useClickOutside";
 import useIntersection from "./useIntersection";
 import useResize from "./useResize";
 import useMutation from "./useMutation";
-import useUpdate from "./useUpdate";
+import useEvent from "./useEvent";
 
 export {
     useDidMount,
@@ -24,4 +27,6 @@ export {
     useResize,
     useMutation,
     useUpdate,
+    useIsFirstMount,
+    useEvent,
 };
